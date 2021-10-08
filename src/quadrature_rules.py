954f43c21d57689b960b7177d4dba2fc0d48719b
axeldinh <axeldvc@gmail.com>
2021-10-09 01:20:37 +0200

    Add Integrate

--- src/quadrature_rules.py
+++ src/quadrature_rules.py
@@ -23,3 +23,33 @@ def GaussLobattoQuadrature(num_points, a, b):
                              np.array([2/(num_points*(num_points-1))])])
     roots = (b-a)/2 * (roots+1) + a
     return roots, weights
+
+def Integrate(f, a, b, roots = None, weights = None):
+    '''
+    Integrates a function f over [a, b] using the given roots and weights
+    Parameters:
+        f (iterable or function handle): if f is an array of the same size as weights,
+                                         the integral is directly computed using summation.
+                                         Otherwise f is evaluated on thee roots before hand.
+        a (float)                      : start of the interval
+        b (float)                      : end of the interval
+        roots (iterable)               : where the function needds to be evaluated (as accorded to the quadrature rule)
+        weights (iterable)             : weights for the quadrature
+    '''
+    
+    if weights is None:
+        raise ValueError("No quadrature weights provided (weights = None)")
+        
+    if type(f).__name__ == 'function':
+        
+        if roots is None:
+            raise ValueError("f is a function handle but no values were provided for evaluation (roots = None)")
+        
+        xs = f(roots)
+        
+    else:
+        xs = f
+        
+    integral = (b-a) * np.sum([x*w for x, w in zip(xs, weights)]) / 2
+    
+    return integral
