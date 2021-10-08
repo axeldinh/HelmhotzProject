3d278d2022232a4971e4ee29a493bd613d41be19
axeldinh <axeldvc@gmail.com>
2021-10-09 00:00:04 +0200

    Add torch_derivatives + tests

---
+++ src/torch_derivatives.py
@@ -0,0 +1,94 @@
+import torch
+
+def compute_derivative(u, x, retain_graph = False, create_graph = False, allow_unused = False):
+    '''
+    Computes the derivative of u with respect to x. 
+    E.g.: If u = [u1, u2, u3], and x = [x1, x2, x3],
+    the function will return u_x = [du1/dx(x1), du2/dx(x2), du3/dx(x3), du3/dx(x3)]
+    
+        Parameters:
+            u (torch.Tensor): Tensor of size (x.size(0)), it corresponds to the function needed to be derived
+            x (torch.Tensor): Tensor of size (number of samples, D), where D is the dimension of the problem.
+                            x should require grad (x.requires_grad = True).
+            retain_graph (Bool): Parameter for the torch.autograd.backward function
+            create_graph (Bool): Parameter for the torch.autograd.backward function
+        
+        Example:
+            x = torch.arange(12).view(4,3).float()
+            x.requires_grad = True
+            u = torch.sum(x**2, dim = 1)
+            u_x = compute_derivative(u, x)
+    '''
+
+    # CHECKS
+    #########################################
+    if not x.requires_grad:
+        raise ValueError("x.requires_grad is set to False")
+        
+    if len(x.size()) > 2 and x.size(1) > 1:
+        raise ValueError(f"x is a {len(x.size())}D Tensor, please choose a 1D Tensor. x has size {x.size()}")
+
+    if len(u.size()) > 2 and u.size(1) > 1:
+        raise ValueError(f"u is a {len(u.size())}D Tensor, please choose a 2D Tensor (u should return a scalar per point). u has size {u.size()}")
+
+    if len(u.size()) > 1:
+        if u.size(1) > 1:
+            raise ValueError(f"More than one value is associated to each point, u should either be a 1D Tensor or a flat 2D Tensor,  u has size {u.size()}")
+
+    grad = torch.ones(u.size())
+    u_x = torch.autograd.grad([u], inputs = x, grad_outputs=[grad],
+                              retain_graph=retain_graph, create_graph=create_graph, allow_unused=allow_unused)[0]
+    if u_x==None:
+        u_x = torch.zeros(x.size())
+    
+    return u_x
+
+def compute_laplacian(u, inputs):
+    '''
+    Computes the Laplacian of u with respect to x. 
+    E.g.: If u = [u1, u2, u3], and x = [[x11, x12], [x21, x22]],
+    the function will return u_x = [d^2u1/dx11^2 + d^2u1/dx12^2, d^2u2/dx21^2 + d^2u2/dx22^2]
+    
+        Parameters:
+            u (torch.Tensor): Tensor of size (x.size(0)), it corresponds to the function needed to be derived
+            inputs (sequence of torch.Tensor): Sequence of D Tensors of size (number of samples,), 
+                                               where D is the dimension of the problem.
+                                               Each Tensor should require grad (x.requires_grad = True).
+            retain_graph (Bool): Parameter for the torch.autograd.backward function
+            create_graph (Bool): Parameter for the torch.autograd.backward function
+                            
+    Example:
+        x = torch.arange(4).view(-1,1).float()
+        y = torch.arange(4).view(-1,1).float()
+        x.requires_grad = True
+        y.requires_grad = True
+        u = x**2 + y**2
+        u_lapl = compute_laplacian(u, [x,y])
+    '''
+
+    # CHECKS
+    #########################################
+
+    for x in inputs:
+        if not x.requires_grad:
+            raise ValueError("x.requires_grad is set to False")
+            
+        if len(x.size()) > 2 and x.size(1) > 1:
+            raise ValueError(f"x is a {len(x.size())}D Tensor, please choose a 1D Tensor. x has size {x.size()}")
+
+    if len(u.size()) > 3:
+        raise ValueError(f"u is a 3D Tensor, please choose a 2D Tensor (u should return a scalar per point). u has size {u.size()}")
+
+    if len(u.size()) > 1:
+        if u.size(1) > 1:
+            raise ValueError(f"More than one value is associated to each point, u should either be a 1D Tensor or a flat 2D Tensor,  u has size {u.size()}")
+        
+    lapl = []
+
+    for x in inputs:
+        u_clone = u.clone()
+        u_x = compute_derivative(u_clone, x, retain_graph=True, create_graph=True)
+        u_xx = compute_derivative(u_x, x, allow_unused=True)
+        lapl.append(u_xx.view(-1, 1))
+        
+    return torch.sum(torch.cat(lapl, dim = 1), dim = 1)
---
+++ test/test_torch_derivatives.py
@@ -0,0 +1,138 @@
+import sys
+sys.path.append("./src")
+
+import torch_derivatives
+
+import unittest
+import torch
+
+class Test_compute_derivatives(unittest.TestCase):
+
+    def test_square(self):
+        '''
+        Test the compute_derivative(u,x) function with u = x**2
+        '''
+    
+        x = torch.arange(3).view(-1,1).float()
+        x.requires_grad = True
+        u = x**2
+        u_x = torch_derivatives.compute_derivative(u, x)
+        expected = 2*x.clone()
+        self.assertEqual(torch.all(torch.eq(u_x, expected)), True, msg=f"\n\nu_x should be \n{expected} \nbut got\n {u_x}")
+
+    def test_cube(self):
+        '''
+        Test the compute_derivative(u,x) function with u = x1**3 + x2**3 + x3**3
+        '''
+    
+        x = torch.arange(12).view(4,3).float()
+        x.requires_grad = True
+        u = torch.sum(x**3, dim = 1)
+        u_x = torch_derivatives.compute_derivative(u, x)
+        expected = (3*x**2).clone()
+        self.assertEqual(torch.all(torch.eq(u_x, expected)), True, msg=f"\n\nu_x should be \n{expected} \nbut got\n {u_x}")
+
+    def test_exp(self):
+        '''
+        Test the compute_derivative(u,x) function with u = exp(x1) + exp(x2) + exp(x3)
+        '''
+    
+        x = torch.arange(12).view(4,3).float()
+        x.requires_grad = True
+        u = torch.sum(torch.exp(x), dim = 1)
+        u_x = torch_derivatives.compute_derivative(u, x)
+        expected = torch.exp(x).clone()
+        self.assertEqual(torch.all(torch.eq(u_x, expected)), True, msg=f"\n\nu_x should be \n{expected} \nbut got\n {u_x}")
+
+    def test_mult(self):
+        '''
+        Test the compute_derivative(u,x) function with u = x1*x2
+        '''
+    
+        x = torch.arange(8).view(4,2).float()
+        x.requires_grad = True
+        u = x[:,0]*x[:,1]
+        u_x = torch_derivatives.compute_derivative(u, x)
+        expected = x[:, [1,0]].clone()
+        self.assertEqual(torch.all(torch.eq(u_x, expected)), True, msg=f"\n\nu_x should be \n{expected} \nbut got\n {u_x}")
+
+class Test_compute_laplacian(unittest.TestCase):
+
+    def test_square(self):
+        '''
+        Test the compute_laplacian(u,x) function with u = x1**2 + x2**2 + x3**2
+        '''
+
+        x = torch.arange(4).view(-1,1).float()
+        y = torch.arange(4,8).view(-1,1).float()
+        z = torch.arange(8,12).view(-1,1).float()
+        x.requires_grad = True
+        y.requires_grad = True
+        z.requires_grad = True
+
+        u = x**2 + y**2 + z**2
+        u_lapl = torch_derivatives.compute_laplacian(u, [x, y, z])
+
+        expected = torch.sum(2*torch.ones((4, 3)), dim = 1)
+        self.assertEqual(torch.all(torch.eq(u_lapl, expected)), True, msg=f"\n\nu_lapl should be \n{expected} \nbut got\n {u_lapl}")
+
+    def test_cube(self):
+        '''
+        Test the compute_laplacian(u,x) function with u = x1**3 + x2**3 + x3**3
+        '''
+
+        x = torch.arange(4).view(-1,1).float()
+        y = torch.arange(4,8).view(-1,1).float()
+        z = torch.arange(8,12).view(-1,1).float()
+        x.requires_grad = True
+        y.requires_grad = True
+        z.requires_grad = True
+
+        u = x**3 + y**3 + z**3
+        u_lapl = torch_derivatives.compute_laplacian(u, [x, y, z])
+
+        expected = torch.sum(torch.cat([6*x, 6*y, 6*z], dim = 1), dim = 1).clone()
+        self.assertEqual(torch.all(torch.eq(u_lapl, expected)), True, msg=f"\n\nu_lapl should be \n{expected} \nbut got\n {u_lapl}")
+
+    def test_exp(self):
+        '''
+        Test the compute_laplacian(u,x) function with u = exp(x1) + exp(x2) + exp(x3)
+        '''
+
+        x = torch.arange(4).view(-1,1).float()
+        y = torch.arange(4,8).view(-1,1).float()
+        z = torch.arange(8,12).view(-1,1).float()
+        x.requires_grad = True
+        y.requires_grad = True
+        z.requires_grad = True
+
+        u = torch.exp(x) + torch.exp(y) + torch.exp(z)
+        u_lapl = torch_derivatives.compute_laplacian(u, [x, y, z])
+
+        expected = u.clone().view(u_lapl.size())
+        self.assertEqual(torch.all(torch.eq(u_lapl, expected)), True, msg=f"\n\nu_lapl should be \n{expected} \nbut got\n {u_lapl}")
+
+    def test_mult(self):
+        '''
+        Test the compute_lalacian(u,x) function with u = x1*x2
+        '''
+
+        x = torch.arange(4).view(-1,1).float()
+        y = torch.arange(4,8).view(-1,1).float()
+        z = torch.arange(8,12).view(-1,1).float()
+        x.requires_grad = True
+        y.requires_grad = True
+        z.requires_grad = True
+
+        u = x * y * z
+        u_lapl = torch_derivatives.compute_laplacian(u, [x, y, z])
+
+        expected = torch.zeros((4,)).view(u_lapl.size())
+        self.assertEqual(torch.all(torch.eq(u_lapl, expected)), True, msg=f"\n\nu_lapl should be \n{expected} \nbut got\n {u_lapl}")
+
+
+if __name__=="__main__":
+
+    unittest.main()
+
+
