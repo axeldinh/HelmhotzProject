8088fb889b8e8338f39ea4f0b7f088758e5cd6bc
axeldinh <axeldvc@gmail.com>
2021-10-09 18:11:21 +0200

    Import torch

--- src/train.py
+++ src/train.py
@@ -1,16 +1,16 @@
 from tqdm import tqdm
 import torch
 
-def train(vpinn, optimizer, num_iter):
+def train(model, optimizer, num_iter):
     t = tqdm(range(num_iter))
     last_loss = 0
     for iter_ in t:
         optimizer.zero_grad()
-        loss_interior, loss_boundary = vpinn.compute_loss(3)
+        loss_interior, loss_boundary = model.compute_loss(3)
         loss = loss_interior + loss_boundary
         loss.backward(retain_graph=True)
-        for n, p in vpinn.model.named_parameters():
-            vpinn.grad_parameters[n].append(p.grad.clone())
+        for n, p in model.model.named_parameters():
+            model.grad_parameters[n].append(p.grad.clone())
         optimizer.step()
         t.set_description("Loss Interior = {:.7f}, Loss Boundary = {:.7f}".format(loss_interior.item(), loss_boundary.item()))
         if torch.abs(last_loss - loss) < 1e-10:
