53f20aaa5df5e4b757dac1225809a150bd0b912f
axeldinh <axeldvc@gmail.com>
2021-10-09 17:23:31 +0200

    train file start

---
+++ src/train.py
@@ -0,0 +1,16 @@
+def train(vpinn, optimizer, num_iter):
+    t = tqdm(range(num_iter))
+    last_loss = 0
+    for iter_ in t:
+        optimizer.zero_grad()
+        loss_interior, loss_boundary = vpinn.compute_loss(3)
+        loss = loss_interior + loss_boundary
+        loss.backward(retain_graph=True)
+        for n, p in vpinn.model.named_parameters():
+            vpinn.grad_parameters[n].append(p.grad.clone())
+        optimizer.step()
+        t.set_description("Loss Interior = {:.7f}, Loss Boundary = {:.7f}".format(loss_interior.item(), loss_boundary.item()))
+        if torch.abs(last_loss - loss) < 1e-10:
+            break
+        last_loss = loss
+    print("Training Over")
