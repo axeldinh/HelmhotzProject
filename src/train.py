cbcc634b89edd7a0b07685d179a9df3a95d66164
axeldinh <axeldvc@gmail.com>
2021-10-13 23:15:24 +0200

    Option to save gradients during training

--- src/train.py
+++ src/train.py
@@ -3,10 +3,15 @@ from tqdm import tqdm
 import torch
 import copy
 
-def train(model, optimizer, num_iter, method = 1, tol = 1e-10):
+def train(model, optimizer, num_iter, method = 1, tol = 1e-10, save_gradients = False):
+    """
+    Runs the model num_iter times, it ends if the loss stops converging (with a tolerance of tol)
+    During the process, the best model is saved and fed into the model at the the end of the training.
+    """
     t = tqdm(range(num_iter))
     last_loss = 0
     best_loss = np.inf
+
     for iter_ in t:
         if optimizer is not None:
             optimizer.zero_grad()
@@ -17,7 +22,9 @@ def train(model, optimizer, num_iter, method = 1, tol = 1e-10):
         if loss < best_loss:
             best_loss = loss
             model.best_model = copy.deepcopy(model.model)
-        if type(model.model).__name__ != "function":
+
+        # If we want we can save the gradients
+        if type(model.model).__name__ != "function" and save_gradients:
             for n, p in model.model.named_parameters():
                 model.grad_parameters[n].append(p.grad.clone())
 
